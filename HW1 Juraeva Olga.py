ca8b0dcb4d8ab7de006f01549d45098afe374ebb
Olya0596 <43132291+Olya0596@users.noreply.github.com>
2018-09-16 23:12:14 +0300

    Add files via upload

---
+++ HW1 Juraeva Olga.py
@@ -0,0 +1,21 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> #1
+>>> str = "угадайка"
+>>> print(str[1])
+г
+>>> print(str[3])
+д
+>>> print(str[5])
+й
+>>> 
+>>> 
+>>> #3
+>>> str = "красивый"
+>>> print(str [0:4])
+крас
+>>> print(str [-1:5])
+
+>>> 
